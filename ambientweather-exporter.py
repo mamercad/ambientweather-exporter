fdc6d117adb9b28d8112b6ae9f07ef53ac49b85c
Mark Mercado <mamercad@gmail.com>
2020-09-09 06:44:35 -0400

    Working on adding Influx support for @silfreed

--- ambientweather-exporter.py
+++ ambientweather-exporter.py
@@ -10,35 +10,73 @@ from flask import Flask
 app = Flask(__name__)
 
 
+applicationKey = os.getenv('AMBI_APP_KEY')
+apiKey         = os.getenv('AMBI_API_KEY')
+
+
 @app.route('/')
 @app.route('/metrics')
-def hello():
-    data = ambientweather()
-    return(data)
+def prometheus():
+    return(ambientweatherPrometheus())
 
 
-def ambientweather():
-    data = []
+@app.route('/influx')
+@app.route('/influxdb')
+def influx():
+    return(ambientweatherInflux())
+
+
+def getData():
+    global applicationKey, apiKey
     try:
-        applicationKey = os.getenv('AMBI_APP_KEY')
-        apiKey         = os.getenv('AMBI_API_KEY')
         r = requests.get(f'https://api.ambientweather.net/v1/devices?applicationKey={applicationKey}&apiKey={apiKey}')
-        results = r.json()[0]
-        labels = 'macAddress="{}",name="{}",lat="{}",lon="{}",address="{}",location="{}",tz="{}"'.format(
-            results['macAddress'],
-            results['info']['name'],
-            results['info']['coords']['coords']['lat'],
-            results['info']['coords']['coords']['lon'],
-            results['info']['coords']['address'],
-            results['info']['coords']['location'],
-            results['lastData']['tz']
-        )
-        for k in results['lastData'].keys():
-            if k not in ['lastRain', 'tz', 'date']:
-                data.append('ambientweather_{}{{{}}} {}'.format(k, labels, results['lastData'][k]))
+        return(r.json()[0])
     except Exception as e:
-        data.append(e)
-    return("\n".join(data))
+        print("Couldn't fetch AmbientWeather data, bailing!")
+        sys.exit(1)
+
+
+def ambientweatherPrometheus():
+    retn = []
+    data = getData()
+    try:
+        labels = 'macAddress="{}",name="{}",lat="{}",lon="{}",address="{}",location="{}",tz="{}"'.format(
+            data['macAddress'],
+            data['info']['name'],
+            data['info']['coords']['coords']['lat'],
+            data['info']['coords']['coords']['lon'],
+            data['info']['coords']['address'],
+            data['info']['coords']['location'],
+            data['lastData']['tz']
+        )
+        for k in data['lastData'].keys():
+            if k not in ['lastRain', 'tz', 'date']:
+                retn.append('ambientweather_{}{{{}}} {}'.format(k, labels, data['lastData'][k]))
+    except Exception as e:
+        retn.append(e)
+    return("\n".join(retn))
+
+
+def ambientweatherInflux():
+    retn = []
+    data = getData()
+    try:
+        tagset = 'macAddress={},name={},lat={},lon={},address={},location={},tz={}'.format(
+            data['macAddress'],
+            data['info']['name'],
+            data['info']['coords']['coords']['lat'],
+            data['info']['coords']['coords']['lon'],
+            data['info']['coords']['address'],
+            data['info']['coords']['location'],
+            data['lastData']['tz']
+        )
+        for k in data['lastData'].keys():
+            if k not in ['lastRain', 'tz', 'date']:
+                # retn.append('ambientweather,{} {}={}'.format(tagset, k, data['lastData'][k]))
+                retn.append('ambientweather {}={}'.format(k, data['lastData'][k]))
+    except Exception as e:
+        retn.append(e)
+    return("\n".join(retn))
 
 
 if __name__ == '__main__':
